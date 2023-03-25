3468a965b33dc90ba88f78e42e5e76a85af25095
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-03-25 13:05:54 +0100

    added coin images

--- levels/level1.js
+++ levels/level1.js
@@ -38,6 +38,16 @@ const level1 = new Level(
         new Bottle()
     ],
     [
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin(),
+        new Coin()
+    ],
+    [
         new BackgroundObject('./img/5_background/layers/air.png', -719),
         new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', -719),
         new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', -719),
